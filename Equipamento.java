89b06a7c39bc6460425f7466c7e38fda48807065
Gabriel Ferreira <53950364+thegabriew@users.noreply.github.com>
2024-02-15 16:08:36 -0300

    Criação da classe "Equipamento"

---
+++ Equipamento.java
@@ -0,0 +1,38 @@
+private class Equipamento {
+
+  private String nome;
+  private int codEquipamento;
+  private String tipo;
+  private String dataCompra;
+
+  void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  String getNome() {
+    return this.nome;
+  }
+
+  void setCodEquipamento(int codEquipamento) {
+    this.codEquipamento = codEquipamento;
+  }
+
+  int getCodEquipamento() {
+    return this.codEquipamento;
+  }
+
+  void setTipo(String tipo) {
+    this.tipo = tipo;
+  }
+
+  String getTipo() {
+    return this.tipo;
+  }
+
+  void setDataCompra(String dataCompra) {
+    this.dataCompra = dataCompra;
+  }
+
+  String getDataCompra(){
+    return this.dataCompra;
+}
