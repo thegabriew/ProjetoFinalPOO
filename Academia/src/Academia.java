af3086099c3c4d08b78be649e6bf8da8866d8a85
Gabriel Ferreira <53950364+thegabriew@users.noreply.github.com>
2024-02-22 16:19:01 -0300

    Criando classe "Academia"

---
+++ Academia/src/Academia.java
@@ -0,0 +1,74 @@
+import java.util.Scanner;
+
+public class Academia {
+  
+  Funcionario f = new Funcionario();      
+  
+  private String nome;
+  private String cnpj;
+  private String UF;
+  private int telefone;
+  private String f.nomeGerente;
+ 
+
+  
+
+  void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  String getNome() {
+    return this.nome;
+  }
+
+  void setCnpj(String cnpj) {
+    this.cnpj = cnpj;
+  }
+
+  String getCnpj() {
+    return this.cnpj;
+  }
+
+  void setUF(String UF) {
+    this.UF = UF;
+  }
+
+  String getUF() {
+    return this.UF;
+  }
+
+  void setTelefone(int telefone) {
+    this.telefone = telefone;
+  }
+
+  int getTelefone() {
+    return this.telefone;
+  }
+
+  void registrarAcademia() {
+    Scanner scan = new Scanner (System.in);
+    System.out.println("*** REGISTRAR ACADEMIA ***");
+    System.out.println("Digite o nome da academia: ");
+    nome = scan.nextLine();
+    System.out.println("Digite o CNPJ da academia: ");
+    cnpj = scan.nextLine();
+    System.out.println("Digite a UF da academia: ");
+    UF = scan.nextLine();
+    System.out.println("Digite o telefone da academia: ");
+    telefone = scan.nextInt();
+    System.out.println("Digite o nome do gerente: ");
+    f.nomeGerente = scan.nextLine();
+    System.out.println("Academia registrada");
+  }
+
+  void exibirAcademia(){
+    System.out.println("*** INFORMAÇÕES DA ACADEMIA ***");
+    System.out.println("Nome: " + nome);
+    System.out.println("CNPJ: " + cnpj);
+    System.out.println("UF: " + UF);
+    System.out.println("Telefone: " + telefone);
+    System.out.println("Nome do gerente: " + f.nomeGerente);
+    
+  }
+
+}
