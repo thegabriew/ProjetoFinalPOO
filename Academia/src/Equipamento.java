d33503faef2ffceb82978a772dc33414209e635d
Gabriel Ferreira <53950364+thegabriew@users.noreply.github.com>
2024-02-27 14:18:15 -0300

    Update Equipamento

--- Academia/src/Equipamento.java
+++ Academia/src/Equipamento.java
@@ -1,5 +1,3 @@
-package Academia.src;
-
 import java.util.Scanner;
 
 public class Equipamento {
@@ -64,6 +62,7 @@ public class Equipamento {
     tipo = input.nextLine();
     System.out.println("Data de compra: ");
     dataCompra = input.nextLine();
+    input.close();
   }
 
   void exibirEquipamento() {
